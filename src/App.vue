e0eeaf118eeb84c66bb96693e8be39942fa5fe47
Anna <anna.cano.7e7@itb.cat>
2024-11-21 17:37:25 +0100

    add:  watch examples

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import KeyForSample from './components/KeyForSample.vue'
+import WatchArrayExample from './components/WatchArrayExample.vue';
 </script>
 
 <template>
   <KeyForSample></KeyForSample>
+  <WatchArrayExample></WatchArrayExample>
 </template>
 
 <style scoped></style>
--- src/components/WatchArrayExample.vue
+++ src/components/WatchArrayExample.vue
@@ -1,11 +1,26 @@
 <script setup>
 import { ref, watch } from 'vue'
+
 const numbers = ref([])
-
-watch(numbers, () => console.log('new number added'))
-
+watch(numbers, (numbers) => console.log(numbers),{deep:true})
 const randomNum = () => numbers.value.push(Math.random())
+
 </script>
 <template>
   <button @click="randomNum">Add Random Number</button>
 </template>
+
+<!--
+//Example with text:
+
+<script setup>
+import { ref, watch } from 'vue'
+const text =ref('')
+watch (text, (newText, oldText) => console.log(oldText, newText))
+
+</script>
+
+<template>
+  <input type="text" v-model="text">
+</template>
+-->
