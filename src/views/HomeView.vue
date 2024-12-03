90aaeb7c484b5a9ba7a8fed0221f84d200ec809c
Anna <anna.cano.7e7@itb.cat>
2024-12-03 17:34:38 +0100

    add: change file structure, add new example for computed()

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,13 @@
 <script setup>
-import NameSurnameExample from '@/components/NameSurnameExample.vue';
+import NameSurnameExample from '@/components/computed/NameSurnameExample.vue';
+import PriceProduct from '@/components/computed/PriceProduct.vue';
 
 
 </script>
 
 <template>
 <h1>Home</h1>
+<h2>Computed:</h2>
 <NameSurnameExample></NameSurnameExample>
+<PriceProduct></PriceProduct>
 </template>
