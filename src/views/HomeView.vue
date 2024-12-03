e19a9fb53b631cbd8c2b26c8898c66abff259a87
Anna <anna.cano.7e7@itb.cat>
2024-12-03 17:23:13 +0100

    add: computed example

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,10 @@
 <script setup>
+import NameSurnameExample from '@/components/NameSurnameExample.vue';
+
 
 </script>
 
 <template>
 <h1>Home</h1>
+<NameSurnameExample></NameSurnameExample>
 </template>
